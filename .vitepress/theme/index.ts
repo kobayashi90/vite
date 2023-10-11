e8e2c153de6b8cb2da76fd56bee909d8c2a75296
taskylizard <75871323+taskylizard@users.noreply.github.com>
2023-10-11 07:15:20 +0200

    add image zoom
    Signed-off-by: taskylizard <75871323+taskylizard@users.noreply.github.com>

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,16 +4,31 @@ import Theme from "vitepress/theme";
 import "./style.css";
 import { enhanceAppWithTabs } from "vitepress-plugin-tabs/client";
 import "uno.css";
-
+import { onMounted, watch, nextTick } from "vue";
+import { useRoute } from "vitepress";
+import mediumZoom from "medium-zoom";
 
 export default {
   extends: Theme,
-  Layout: () =>{
+  Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     });
   },
   enhanceApp({ app, router, siteData }) {
     enhanceAppWithTabs(app);
+  },
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      mediumZoom(".main img", { background: "var(--vp-c-bg)" });
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom()),
+    );
   },
 };
